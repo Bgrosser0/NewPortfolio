acea187b13fba63a631d8a5d77f9f3fef5b8090a
ben grosser <bgrosser0@mncallysmith.edu>
2018-07-24 01:13:39 -0500

    more progress on about me page, added tabs to projects and added template to contact

--- np1/src/pages/Projects.js
+++ np1/src/pages/Projects.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './pagesCSS/project.css';
 
 const Projects = () => (
-    <div>
-      <h1>Projects</h1>
-    </div>
+  <Tabs>
+    <TabList>
+      <Tab>Front End</Tab>
+      <Tab>Back End</Tab>
+    </TabList>
+
+    <TabPanel>
+        <div className="FEcard">
+      <h2>Front End Projects</h2>
+        </div>
+    </TabPanel>
+    <TabPanel>
+        <div className="BEcard">
+      <h2>Back End Projects</h2>
+        </div>
+    </TabPanel>
+  </Tabs>
 );
 
 export default Projects;
