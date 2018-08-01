fa8cf439e13019dc0b6baf6ff4904733d4079908
ben grosser <bgrosser0@mncallysmith.edu>
2018-08-01 17:37:59 -0500

    Added project descriptions

--- np1/src/components/ProjectCard2/ProjectCard2.js
+++ np1/src/components/ProjectCard2/ProjectCard2.js
@@ -23,7 +23,10 @@ const ProjectCard2 = props => (
               The Troll's Toll
             </h1>
             <p className="introText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies consequat auctor. Donec convallis ornare egestas. Quisque placerat, augue pulvinar tristique tristique, purus nulla dignissim est, mollis mollis nisi lorem ut magna. Praesent efficitur pretium nunc vitae eleifend. Sed pharetra metus imperdiet, fringilla mi sed, maximus quam. Praesent nisi lectus, cursus id tincidunt sit amet, dictum vestibulum enim. Integer pharetra pellentesque nulla, malesuada feugiat lacus semper et. Nam egestas gravida eros et volutpat. Nunc laoreet convallis sagittis. Praesent sit amet arcu sed quam viverra dapibus. Phasellus pharetra nisl et faucibus tempus. Integer nec orci a velit dictum fringilla. Sed viverra feugiat elit congue egestas. Nunc non libero nisl.
+            	A choose your own adventure game, the player moves through a story making choices that lead to branching paths with different story content and endings.
+            </p>
+            <p>
+            	Developed using HandleBars, JavaScript, Express, MySQL and Sequelize.
             </p>
             </Col>    
           </Row>
--- np1/src/components/ProjectCard3/ProjectCard3.js
+++ np1/src/components/ProjectCard3/ProjectCard3.js
@@ -23,12 +23,16 @@ const ProjectCard3 = props => (
               Liri
             </h1>
             <p className="introText">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies consequat auctor. Donec convallis ornare egestas. Quisque placerat, augue pulvinar tristique tristique, purus nulla dignissim est, mollis mollis nisi lorem ut magna. Praesent efficitur pretium nunc vitae eleifend. Sed pharetra metus imperdiet, fringilla mi sed, maximus quam. Praesent nisi lectus, cursus id tincidunt sit amet, dictum vestibulum enim. Integer pharetra pellentesque nulla, malesuada feugiat lacus semper et. Nam egestas gravida eros et volutpat. Nunc laoreet convallis sagittis. Praesent sit amet arcu sed quam viverra dapibus. Phasellus pharetra nisl et faucibus tempus. Integer nec orci a velit dictum fringilla. Sed viverra feugiat elit congue egestas. Nunc non libero nisl.
-            </p>
+            Command line node app to return tweets, song information or movie information.             </p>
             </Col>    
           </Row>
           <Row>
+            <Col size="md-6">
           <button className="btn btn-primary" id="LiriButton" data-toggle="modal" data-target="#newpassword" ><a target="_blank" rel="noopener noreferrer" href="https://github.com/Bgrosser0/Liri">GitHub</a></button>
+            </Col>
+            <Col size="md-6">
+              <br></br>
+            </Col>
           </Row>
         </Container>
     </div>
