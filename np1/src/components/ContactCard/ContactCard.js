758554f65c902cb361367dfe3db8187d85fecac7
ben grosser <bgrosser0@mncallysmith.edu>
2018-08-01 17:29:19 -0500

    updatedStuff

--- np1/src/components/ContactCard/ContactCard.js
+++ np1/src/components/ContactCard/ContactCard.js
@@ -7,7 +7,7 @@ import Container from "../Grid/Container";
 
 const ContactCard = props => (
   <div className="card" id="contactCard">
-    <div className="card-body">
+    <div className="card-body" id="contactCardBody">
      
           <Row>
             <Col size="md-12">
@@ -19,35 +19,39 @@ const ContactCard = props => (
           </Row>
 
           <Row>
-          <Col size="md-6">
+          <Col size="md-12">
+          <div className="ContactInfo">
           <p>
-            Phone: 612-709-4778
+          <i class="fas fa-phone"></i> Phone: 612-709-4778
           </p>
           <p>
-            Email: Bgrosser00@gmail.com
+          <i class="fas fa-envelope"></i> Email: Bgrosser00@gmail.com
           </p>
           <p>
-            Location: Minneapolis, Minnesota
+          <i class="fas fa-map-marker-alt"></i>  Location: Minneapolis, Minnesota
           </p>
+          </div>
           </Col>
-    <Col size="md-6">
+    {/* <Col size="md-6">
+    <div className="ContactForm">
     <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Name</label>
+  <div className="form-group" id="NameForm">
+    <label for="exampleInputEmail1" className="FormLable">Name</label>
     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name"></input>
   </div>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+  <div className="form-group" id="EmailForm">
+    <label for="exampleInputEmail1" className="FormLable">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email"></input>
   </div>
   <div className="form-group">
-    <label for="exampleFormControlTextarea1">Message</label>
+    <label for="exampleFormControlTextarea1" className="FormLable">Message</label>
     <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
   <button type="submit" className="btn btn-primary">Submit</button>
   
 </form>
-</Col>
+</div>
+</Col> */}
   </Row>
 
 
