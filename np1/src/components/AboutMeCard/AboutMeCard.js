977fe83db01c5e84d35f2fed16d11565d561486f
ben grosser <bgrosser0@mncallysmith.edu>
2018-08-24 14:05:06 -0500

    added pic to aboutMe page and updated responsiveness

--- np1/src/components/AboutMeCard/AboutMeCard.js
+++ np1/src/components/AboutMeCard/AboutMeCard.js
@@ -11,19 +11,24 @@ const AboutMeCard = props => (
         <Container>
           <Row>
             <Col size="md-4">
+            <div id="pic-hold">
             <img
     className="avatar-img"
-    src="https://www.shareicon.net/data/512x512/2015/09/18/103160_man_512x512.png"
+    src="https://farm2.staticflickr.com/1882/42434850600_7e1a9baed4_m.jpg"
     alt="avatar"
+    id="abtPic"
     />
+    </div>
             </Col>
             <Col size="md-8">
+            <div id="content-hold">
             <h1>
               About Me
             </h1>
             <p className="introText">
             My name is Ben Grosser.  I am a full stack developer currently located in Minneapolis, Minnesota.  After getting a degree in music composition I became interested in programming to further my skills as a music producer and fell in love with coding through a coding bootcamp at the University of Minnesota. I developed an interest and respect for front-end and back-end development and have continued to develop my skills as a programmer through self-education.  I am motivated by new challenges and the pursuit of personal growth. 
             </p>
+            </div>
             </Col>    
           </Row>
         </Container>
